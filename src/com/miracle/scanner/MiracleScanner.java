9beb28751a43dcb708af18f44337530204194563
kipsora <kipsora@gmail.com>
2017-04-02 19:43:34 +0800

    Printer added.

--- src/com/miracle/scanner/MiracleScanner.java
+++ src/com/miracle/scanner/MiracleScanner.java
@@ -28,8 +28,8 @@ public class MiracleScanner {
             parser.reset();
             MiracleASTreeBuilder builder = new MiracleASTreeBuilder();
             walker.walk(builder, parser.miracle());
-            MiracleASTreePrinter printer = new MiracleASTreePrinter();
-            builder.getTree().visit(printer);
+            //MiracleASTreePrinter printer = new MiracleASTreePrinter();
+            //builder.getTree().visit(printer);
         } catch (MiracleException e) {
             System.out.print(e.getMessage());
         }
