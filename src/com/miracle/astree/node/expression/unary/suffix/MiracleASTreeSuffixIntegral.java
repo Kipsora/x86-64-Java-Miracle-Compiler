6fff2c71439c70b4028a076214c1250ae80d8b32
kipsora <kipsora@gmail.com>
2017-04-02 21:46:29 +0800

    Bugs fixed for suffix operations.

--- src/com/miracle/astree/node/expression/unary/suffix/MiracleASTreeSuffixIntegral.java
+++ src/com/miracle/astree/node/expression/unary/suffix/MiracleASTreeSuffixIntegral.java
@@ -3,6 +3,7 @@ package com.miracle.astree.node.expression.unary.suffix;
 import com.miracle.astree.node.expression.MiracleASTreeExpression;
 import com.miracle.astree.node.expression.unary.MiracleASTreeUnaryExpression;
 import com.miracle.astree.visitor.MiracleASTreeVisitor;
+import com.miracle.exceptions.MiracleExceptionLeftValue;
 import com.miracle.exceptions.MiracleExceptionSpecialExpression;
 
 import static com.miracle.scanner.listener.MiracleRuntimeMaintainer.MiracleASTreeINT;
@@ -10,6 +11,9 @@ import static com.miracle.scanner.listener.MiracleRuntimeMaintainer.MiracleASTre
 public class MiracleASTreeSuffixIntegral extends MiracleASTreeUnaryExpression {
     public MiracleASTreeSuffixIntegral(OPERATOR operator, MiracleASTreeExpression node) {
         super(operator.toString(), node);
+        if (!node.getMutable()) {
+            throw new MiracleExceptionLeftValue();
+        }
         if (!node.getType().equals(MiracleASTreeINT)) {
             throw new MiracleExceptionSpecialExpression("suffix " + operator.toString(),
                     "int", node.getType().toString());
